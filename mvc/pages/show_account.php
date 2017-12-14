ffbf25d05b8a7fb801a421fc3773b5de2da78dfb
atish anand nayak <an478@afsconnect2.njit.edu>
2017-12-14 15:50:10 -0500

    Display table in Tasks

--- mvc/pages/show_account.php
+++ mvc/pages/show_account.php
@@ -33,11 +33,10 @@ print utility\htmlTable::generateTableFromOneRecord($data);
 <form action="index.php?page=accounts&action=save&id=<?php echo $data->id; ?>" method="post">
 
     First name: <input type="text" name="fname" value="<?php echo $data->fname; ?>"><br>
-
     Last name: <input type="text" name="lname" value="<?php echo $data->lname; ?>"><br>
-    Email: <input type="text" name="email" value="<?php echo $data->email; ?>"><br>
-    Phone: <input type="text" name="phone" value="<?php echo $data->phone; ?>"><br>
-    Birthday: <input type="text" name="birthday" value="<?php echo $data->birthday; ?>"><br>
+    Email:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" name="email" value="<?php echo $data->email; ?>"><br>
+    Phone:&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" name="phone" value="<?php echo $data->phone; ?>"><br>
+    Birthday:&nbsp&nbsp <input type="text" name="birthday" value="<?php echo $data->birthday; ?>"><br>
     Gender: <input type="text" name="gender" value="<?php echo $data->gender; ?>"><br>
     <input type="submit" value="Submit form">
 </form>
