22d269eb66be16d2cd864ebc00e6798015492d3f
atish anand nayak <an478@afsconnect2.njit.edu>
2017-12-14 14:34:53 -0500

    Database structure altered

--- mvc/database.php
+++ mvc/database.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DATABASE', 'kwilliam');
-define('USERNAME', 'kwilliam');
-define('PASSWORD', 'USracpEto');
+define('DATABASE', 'an478');
+define('USERNAME', 'an478');
+define('PASSWORD', '8QD1YLZg');
 define('CONNECTION', 'sql2.njit.edu');
--- mvc/pages/register.php
+++ mvc/pages/register.php
@@ -19,13 +19,13 @@
 
 
 <form action="index.php?page=accounts&action=register" method="post">
-    First name: <input type="text" name="fname"><br>
-    Last name: <input type="text" name="lname"><br>
-    Email: <input type="text" name="email"><br>
-    Phone: <input type="text" name="phone"><br>
-    Birthday: <input type="text" name="birthday"><br>
-    Gender: <input type="text" name="gender"><br>
-    Password: <input type="password" name="password"><br>
+    <em>First name:</em> <input type="text" name="fname"><br><br>
+    <em>Last name:</em> <input type="text" name="lname"><br><br>
+    <em>Email:</em> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input type="text" name="email"><br><br>
+    <em>Phone:</em> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <input type="text" name="phone"><br><br>
+    <em>Birthday:</em> &nbsp&nbsp&nbsp&nbsp<input type="text" name="birthday"><br><br>
+    <em>Gender:</em> &nbsp&nbsp&nbsp&nbsp&nbsp <input type="text" name="gender"><br><br>
+    <em>Password:</em> &nbsp&nbsp&nbsp<input type="password" name="password"><br><br>
     <input type="submit" value="Submit form">
 </form>
 
